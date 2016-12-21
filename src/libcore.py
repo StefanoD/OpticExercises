17d2380e3847896f58b76ee3b6d756fa2306d043
Stefano Di Martino <stefano.d@posteo.de>
2016-12-21 17:00:35 +0100

    Berechnete Konstanten in Quellcode hinzu

--- src/libcore.py
+++ src/libcore.py
@@ -1,6 +1,15 @@
 """Grundfunktionalität für Aufgaben"""
 import numpy as np
 
+"""Berechnete Bestrahlungsstärke E. Siehe Rechnung in Ordner MessungenAufgabe_1-2"""
+IRRADIANCE_WATT_PER_SQUARE_METER = 0.121705797795879
+
+"""Wellenlänge der verwendeten grünen LED"""
+WAVELENGTH_METER = 0.000000525
+
+"""Pixelgröße aus Datenblatt entnommen"""
+PIXEL_AREA_METER = 0.0000045 ** 2
+
 
 def get_time_variance(mat1, mat2):
     """Liefert zeitliche Varianz von zwei Matrizen zurück"""
