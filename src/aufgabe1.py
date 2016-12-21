7ec6cb07700a94684c5e6c5d3bb1d58e48bebe14
Stefano Di Martino <stefano.d@posteo.de>
2016-12-21 18:29:42 +0100

    Konstanten verschoben

---
+++ src/aufgabe1.py
@@ -0,0 +1,9 @@
+"""Berechnete Bestrahlungsstärke E. Siehe Rechnung in Ordner MessungenAufgabe_1-2"""
+IRRADIANCE_WATT_PER_SQUARE_METER = 0.121705797795879
+
+"""Wellenlänge der verwendeten grünen LED"""
+WAVELENGTH_METER = 0.000000525
+
+"""Pixelgröße aus Datenblatt entnommen"""
+PIXEL_AREA_METER = 0.0000045 ** 2
+
